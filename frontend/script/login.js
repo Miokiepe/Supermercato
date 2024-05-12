0df4ab9da30c7457b8a807d8c75742032b203794
Miokiepe <yoshy1048@gmail.com>
2024-05-12 11:01:05 +0200

    Aggiunta files

---
+++ frontend/script/login.js
@@ -0,0 +1,13 @@
+const password = document.querySelector('#password')
+
+const iconEye = document.querySelector('#toggle')
+iconEye.addEventListener('click',()=>{
+        if(password.type === "password") {
+            password.type = "text"
+            iconEye.innerHTML = '<i class="bi bi-eye"></i>'
+        }
+        else {
+            password.type = "password"
+            iconEye.innerHTML = '<i class="bi bi-eye-slash"></i>'
+        }
+})
