eaa6132a51d3a35ebc7d984211d3650def2c731e
Miokiepe <yoshy1048@gmail.com>
2024-05-18 10:10:12 +0200

    Aggiunta files

--- frontend/script/login.js
+++ frontend/script/login.js
@@ -11,3 +11,36 @@ iconEye.addEventListener('click',()=>{
             iconEye.innerHTML = '<i class="bi bi-eye-slash"></i>'
         }
 })
+
+const toggler = document.querySelector('#toggler')
+const image = document.querySelector('#image')
+const select = document.querySelector('#select')
+
+toggler.addEventListener('click',() => {
+    if(toggler.classList.contains('checked')) {
+        toggler.classList.remove('checked')
+        image.style.background = "linear-gradient(45deg, #4D194D, #006466)"
+        select.style.display = "none"
+    }
+    else {
+        toggler.classList.add('checked')
+        image.style.background = "linear-gradient(45deg, #B7094C, #0091AD)"
+        select.style.display = "block"
+    }
+})
+
+const button = document.querySelector('#submit')
+const loader = document.querySelector('#loader')
+button.addEventListener('click',() => {
+    button.disabled = true
+    loader.style.display = "block"
+    const email = document.querySelector('#email').value, password = document.querySelector('#password').value
+    fetch('http://localhost:5000/api/login',{
+        method: "POST",
+        body: JSON.stringify({email: email, password: password})
+    }).then(res => console.log(res)).catch(res=>{
+        alert("ERRORE")
+        button.disabled = false
+        loader.style.display = "none"
+    })
+})
