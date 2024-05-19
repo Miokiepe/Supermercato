d07af3b39be4b9081f67badc5abd3188557362dc
Miokiepe <yoshy1048@gmail.com>
2024-05-19 15:30:38 +0200

    Aggiornamento files

--- frontend/script/registrati.js
+++ frontend/script/registrati.js
@@ -1,12 +1,55 @@
 const registrati = document.querySelector('#registrati')
+const error = document.querySelector('#message')
 registrati.addEventListener('click', () => {
     const 
      nome = document.querySelector('#nome').value,
      cognome = document.querySelector('#cognome').value,
      email = document.querySelector('#email').value,
+     password = document.querySelector('#password').value,
      prefisso = document.querySelector('#prefisso').value,
      numero = document.querySelector('#numero').value,
-     genere = document.querySelector('#genere').value
-
-     console.log(nome,cognome,email,prefisso,numero,genere)
+     genere = document.querySelector('#genere').value,
+     citta = document.querySelector('#città').value,
+     cap = document.querySelector('#cap').value,
+     via = document.querySelector('#via').value
+     registrati.disabled = true;
+     if(!nome || !cognome || !email || !password || !numero || genere == 0 || !citta || !cap || !via) {
+            error.style.display = 'block'
+            error.innerHTML = "Uno o più campi vuoti"
+            registrati.disabled = false;
+            return;
+     }
+     fetch('http://localhost:5000/api/create_account',{
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            id: 0,
+            nome: nome,
+            cognome: cognome,
+            email: email,
+            password: password,
+            prefisso: prefisso, 
+            numero: numero,
+            genere: genere,
+            città: citta,
+            cap: cap,
+            via: via
+        })
+     }).then(res => {
+        if(res.status == 409) {
+            error.style.display = 'block'
+            error.innerHTML = "La mail risulta già registrata. Accedi"
+            return;
+        }
+        setTimeout(()=>{
+            error.className = 'alert alert-success'
+            error.style.display = 'block'
+            error.innerHTML = "Account creato! Clicca su 'accedi'"
+        },2500);
+     }).catch(res => {
+        error.style.display = 'block';
+        registrati.disabled = false;
+     })
 })
