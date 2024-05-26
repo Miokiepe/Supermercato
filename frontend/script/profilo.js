ee5b86f6e44c0ef2541790fc7bd0bfc4e82f735a
Miokiepe <yoshy1048@gmail.com>
2024-05-26 20:33:01 +0200

    agg

--- frontend/script/profilo.js
+++ frontend/script/profilo.js
@@ -1,6 +1,7 @@
 const iconEye = document.querySelector('#toggle')
 const error = document.querySelector('#error')
 const modifica = document.querySelector('#modifica')
+let old_user;
 iconEye.addEventListener('click',()=>{
         if(password.type === "password") {
             password.type = "text"
@@ -22,6 +23,34 @@ modifica.addEventListener('click',() => {
     else {
         //Esegui la richiesta per aggiornare l'account
         modifica.innerHTML = '<i class="fa-solid fa-pen"></i> Modifica'
+        let c = 0
+        IDs.forEach(elem => document.querySelector('#' + elem).value === "" && c++)
+        if(c != 0) {
+            error.style.display = "block"
+            error.innerHTML = "Uno o più campi sono vuoti!"
+            return;
+        }
+        const new_user = {}
+        IDs.forEach(elem => new_user[elem] = document.querySelector('#' + elem).value)
+        new_user['password'] = document.querySelector('#password').value == "" ? old_user.password : document.querySelector('#password').value
+        new_user['autenticato'] = old_user.autenticato
+        new_user['id_utente'] = old_user.id_utente
+        /*fetch('http://localhost:5000/api/update_account',{
+            method: "PUT",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                old: old_user,
+                new: new_user
+            })
+        }).then(() => location.reload())
+          .catch(() => {
+            error.style.display = "block"
+            error.innerHTML = "Qualcosa è andato storto!"
+        })*/
+        console.log(old_user)
+        console.log(new_user)
     }
 })
 
@@ -37,7 +66,7 @@ fetch('http://localhost:5000/api/get_account',{
         role: localStorage.getItem('role')
     })
 }).then(res => res.json()).then(res => {
-    console.log(res)
+    old_user = res
     IDs.forEach(elem => {
         const a = document.querySelector('#' + elem)
         if(a) a.value = res.user[elem]
