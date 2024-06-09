9706fc8e0bb480415f72a85a84a0f775af7f7ee4
Miokiepe <yoshy1048@gmail.com>
2024-06-09 15:56:56 +0200

    Aggiornamento files

--- frontend/Components/data.js
+++ frontend/Components/data.js
@@ -20,6 +20,61 @@ export const items = [{
     icona: '<i class="fa-solid fa-volleyball"></i>'
 }]
 
+export const stati = [
+    {
+        icona: '<i class="fa-solid fa-spinner"></i>',
+        nome: "In preparazione",
+        perc: "0%",
+        titolo: "Pacco in preparazione",
+        desc: "Il pacco è in preparazione per la spedizione."
+    },
+    {
+        icona: '<i class="fa-solid fa-truck"></i>',
+        nome: "Spedito",
+        perc: "25%",
+        titolo: "Pacco spedito",
+        desc: "Il pacco è partito dallo stabilimento in cui si trovava."
+    },
+    {
+        icona: '<i class="fa-solid fa-truck-fast"></i>',
+        nome: "In transito",
+        perc: "50%",
+        titolo: "Pacco in transito",
+        desc: "Il pacco è in transito nei vari centri di smistamento."
+    },
+    {
+        icona: '<i class="fa-solid fa-truck-ramp-box"></i>',
+        nome: "In consegna",
+        perc: "75%",
+        titolo: "Pacco in consegna",
+        desc: "Il pacco è in consegna."
+    },
+    {
+        icona: '<i class="fa-solid fa-circle-check"></i>',
+        nome: "Consegnato",
+        perc: "100%",
+        titolo: "Pacco consegnato",
+        desc: "Il pacco è stato consegnato con successo.",
+        colore: "success"
+    }, 
+    {
+        icona: '<i class="fa-solid fa-circle-xmark"></i>',
+        nome: "Mancata consegna",
+        perc: "75%",
+        titolo: "Mancata consegna",
+        desc: "Non è stato possibile consegnare il pacco. Riproveremo domani alla stessa ora.",
+        colore: "danger"
+    },
+    {
+        icona: '<i class="fa-solid fa-question"></i>',
+        nome: "Smarrito",
+        perc: "0%",
+        titolo: "Pacco smarrito",
+        desc: "Il pacco è stato smarrito. Rivolgiti all'area clienti per assistenza.",
+        colore: "secondary"
+    }
+]
+
 export const show_error = (message = "Impossibile connettersi al server", className = "") => {
     const error = document.querySelector('#error')
     if(className !== "") error.className = className
--- frontend/script/home.js
+++ frontend/script/home.js
@@ -1,14 +1,18 @@
-import {items, show_content, show_error} from '../Components/data.js'
-const prodotti_r = document.querySelector('#prodotti_r')
-const card_t = document.querySelector('template')
-const email = localStorage.getItem('email')
-const password = localStorage.getItem('password')
-const error = document.querySelector('#error')
-const token = localStorage.getItem('token')
-const role = localStorage.getItem('role')
-const id = localStorage.getItem('id');
-const alert_t = localStorage.getItem('alert')
+import {items, show_content, show_error, stati} from '../Components/data.js'
+const  
+    prodotti_r = document.querySelector('#prodotti_r'), 
+    card_t = document.querySelectorAll('template')[0],
+    ordini_t = document.querySelectorAll('template')[1], 
+    email = localStorage.getItem('email'), 
+    password = localStorage.getItem('password'), 
+    error = document.querySelector('#error'), 
+    token = localStorage.getItem('token'), 
+    role = localStorage.getItem('role'), 
+    id = localStorage.getItem('id'), 
+    alert_t = localStorage.getItem('alert')
 localStorage.removeItem('alert')
+
+
 if(alert_t) {
     error.style.display = "block"
     switch(alert_t) {
@@ -30,17 +34,17 @@ const add_cart = (prodotto) => {
             id_utente: parseInt(id),
             id_prodotto: prodotto.id_prodotto,
             quantità: parseInt(document.querySelector('#u' + prodotto.id_prodotto).value)
-        })
+            })
     }).then((res) => {
         if(res.status === 201) {
             localStorage.setItem('alert','cart')
             location.reload()
-        }
-        else show_error()
-        
-    }).catch(() => show_error())
-}
-
+            }
+            else show_error()
+            
+            }).catch(() => show_error())
+            }
+//Renderizzazzione degli ultimi 10 item disponibili
 fetch('http://localhost:5000/api/get_items/10')
     .then(res => res.json())
     .then(res => {
@@ -65,8 +69,8 @@ fetch('http://localhost:5000/api/get_items/10')
                 card.querySelector('.costo').style.visibility = "hidden"
                 card.querySelector(".sep").style.visibility = "hidden"
                 card.querySelector("hr").style.visibility = "hidden"
-            }
-            else {
+                }
+                else {
                 card.querySelector('.quantità').innerHTML = elem.disponibilità
                 card.querySelector('.add_cart').onclick = () => add_cart(elem)
                 card.querySelector('.costo').innerHTML = "€" + elem.costo
@@ -78,35 +82,35 @@ fetch('http://localhost:5000/api/get_items/10')
                 option.value = i
                 option.innerHTML = i
                 select.appendChild(option)
-            }
-            prodotti_r.appendChild(card)
-        })
-    })
-    .catch(() => show_error())
-
+                }
+                prodotti_r.appendChild(card)
+                })
+                })
+                .catch(() => show_error())
+//Renderizzazzione degli ordini
 fetch('http://localhost:5000/api/get_orders_user', {
     method: "POST",
     headers: {
         'Content-Type': 'application/json'
-    },
+        },
     body: JSON.stringify({
         email: email, 
         password: password, 
         token: token, 
         role: role
-    })
-}).then(res => res.json())
-  .then(res => {
-      const container = document.querySelector('#ordini_cards')
-      
-      if(!res.ordini) {
-        container.innerHTML = "Nessun ordine effetuato"
+        })
+        }).then(res => res.json())
+        .then(res => {
+            const container = document.querySelector('#ordini_cards')
+            
+            if(!res.ordini) {
+                container.innerHTML = "Nessun ordine effetuato"
         return;
-      }
+        }
 
-    //riordinare l'arry in base al tipo
-    const res_sorted = res.ordini.sort((a, b) => a.gruppo - b.gruppo)
-
+        //riordinare l'arry in base al tipo
+        const res_sorted = res.ordini.sort((a, b) => a.gruppo - b.gruppo)
+        
     //Creazione di x array per quanti sono i gruppi ordine
     const gruppi = [];
     let gruppo = [], n_gruppo = res_sorted[0].gruppo;
@@ -114,22 +118,53 @@ fetch('http://localhost:5000/api/get_orders_user', {
         if(n_gruppo != elem.gruppo) {
             gruppi.push(gruppo)
             gruppo = []
-        }
-        gruppo.push(elem)
-        n_gruppo = elem.gruppo
-    })
-    gruppi.push(gruppo)
-
+            }
+            gruppo.push(elem)
+            n_gruppo = elem.gruppo
+            })
+            gruppi.push(gruppo)
+    const ordini = document.querySelector('#ordini')
     //Renderizzazzione degli ordini
     gruppi.forEach(array => {
-        const div = document.createElement('div')
-        div.style.border = "2px solid black"
+        const container = document.createElement('div')
+        const container_ordine = document.createElement('div')
+        container_ordine.style = "display: flex; flex-direction: row; gap: 10px; flex-wrap: wrap;"
+        const n_ordine = document.createElement('h3')
+        n_ordine.innerHTML = "N. ordine #"
+        container.appendChild(n_ordine)
+        container.style = "background-color: white; box-shadow: rgba(0, 0, 0, 0.2) 0px 2px 1px -1px, rgba(0, 0, 0, 0.14) 0px 1px 1px 0px, rgba(0, 0, 0, 0.12) 0px 1px 3px 0px; display: flex; flex-wrap: wrap; margin-bottom: 20px; gap: 10px; flex-direction: column; padding: 10px; border-radius: 12px;"
+        console.log(array)
         array.forEach(elem => {
-            const card = document.createElement('p')
-            card.innerHTML = elem.nome
-            div.appendChild(card)
+            const ordine = ordini_t.content.cloneNode(true)
+            ordine.querySelector('.card-title').innerHTML = elem.nome
+            ordine.querySelector('.card-subtitle').innerHTML = `<span style='color: ${items[elem.tipo].colore}'>
+                                                                    ${items[elem.tipo].icona} ${items[elem.tipo].nome}
+                                                                </span>`
+            ordine.querySelector('.ddesc').innerHTML = "Quantità: "  + elem.quantità
+            ordine.querySelector('.costo').innerHTML = '€' + elem.costo
+            const btn = ordine.querySelector('.btn')
+            btn.innerHTML = stati[elem.stato].icona
+            btn.setAttribute('data-bs-title', stati[elem.stato].titolo)
+            btn.setAttribute('data-bs-content', stati[elem.stato].desc)
+            btn.setAttribute('data-bs-toggle','popover')
+            btn.className = "btn btn-" + (stati[elem.stato].colore ? stati[elem.stato].colore : 'primary')
+            ordine.querySelector('.nome').innerHTML = stati[elem.stato].nome
+            ordine.querySelector('.progress-bar').style.width = stati[elem.stato].perc
+            ordine.querySelector('.progress-bar').innerHTML = stati[elem.stato].perc
+            ordine.querySelector('.progress-bar').classList.add("bg-" + (stati[elem.stato].colore ? stati[elem.stato].colore : 'primary'))
+            if(elem.stato == 4) {
+                ordine.querySelector('.progress-bar').classList.remove("progress-bar-animated")
+                ordine.querySelector('.progress-bar').classList.remove("progress-bar-striped")
+            }
+            container_ordine.appendChild(ordine)
+            n_ordine.innerHTML += elem.id_ordine
+            container.appendChild(container_ordine)
         })
-        container.appendChild(div)
-    })
+        ordini.appendChild(container)
+        })
+}).then(() => {
+    //Caricamento dei popover degli ordini
+    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
+    const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
 })
-  .catch(() => show_error())
+.catch((err) => show_error())
