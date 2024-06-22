97cb8fd3a41ba2b2441e6ad9bd597b69bc36ff98
Miokiepe <yoshy1048@gmail.com>
2024-06-22 16:30:39 +0200

    Aggiornamento

--- frontend/script/utenti.js
+++ frontend/script/utenti.js
@@ -3,7 +3,7 @@ const admin_selezionati = [], utenti_selezionati = []
 const elimina = document.querySelector('#elimina_a')
 const modifica = document.querySelector('#modifica_a')
 const elimina_u = document.querySelector('#elimina_u')
-const modifica_m = new bootstrap.Modal(document.getElementById("modifica_m"));
+const modifica_m = bootstrap.Modal(document.getElementById("modifica_m"));
 const elimina_m = new bootstrap.Modal(document.getElementById("elimina_m"));
 const elimina_m_u = new bootstrap.Modal(document.getElementById("elimina_m_u"));
 const aggiungi = document.querySelector('#add')
@@ -186,6 +186,10 @@ const manage_user = (user, check) => {
 //Renderizziamo gl iutenti
 const utenti_t = document.querySelector('#utenti')
 const render_utenti = (utenti) => {
+    if(utenti.length === 0 ) {
+        document.querySelector('#cont_utenti').innerHTML = "Nessun utente"
+        return;
+    }
     utenti.forEach(elem => {
         const tr = document.createElement('tr')
 
